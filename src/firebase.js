0347e3792637dab3f7df2f5299a181d441f5eb5b
musta7188 <musta7188@hotmail.it>
2020-10-13 10:12:07 +0100

    componant structure

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDqoK_CXRFoJNmVjSwZEgvCEciY_3oDR8",
+  authDomain: "clone-project-54181.firebaseapp.com",
+  databaseURL: "https://clone-project-54181.firebaseio.com",
+  projectId: "clone-project-54181",
+  storageBucket: "clone-project-54181.appspot.com",
+  messagingSenderId: "509461564326",
+  appId: "1:509461564326:web:707661ae73aa978ed55d67",
+  measurementId: "G-V715VBNF9X",
+};
