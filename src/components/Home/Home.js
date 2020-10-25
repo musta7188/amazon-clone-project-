53a18f8a3f10d61ed7bc611f734ff3c50e87f94d
musta7188 <musta7188@hotmail.it>
2020-10-25 21:43:01 +0000

    product added to land page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,20 +16,68 @@ export default function Home() {
           />
           <div className="product_container">
           <div className="home__row">
-          <Product title="The lean startup" 
+          <Product
+          id="12342142"
+           title="Pinch of Nom Everyday Light: 100 Tasty, Slimming Recipes All Under 400 Calories Hardcover" 
           price={29.99} 
           image="https://images-na.ssl-images-amazon.com/images/I/51hlRhTeRkL._SX369_BO1,204,203,200_.jpg" rating={5} />
-          <Product/>
+          <Product 
+          id="34324"
+          price={23.99}
+          title="Kenwood kMix stand Mixer for Baking,
+          Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Liter
+          Glass Bowl"
+          rating={4}
+          image="https://johnlewis.scene7.com/is/image/JohnLewis/238134095?$rsp-pdp-port-1080$"
+
+          />
           </div>
 
           <div className="home__row">
-          <Product/>
-          <Product/>
-          <Product/>
+          <Product
+          id="3424234"
+           title="HONOR Watch GS Pro - Multisport Smartwatch with 25-Day Battery Life, Military Standard Certified, GPS, 1.39 inch AMOLED Touch Screen, IP68 Water Resistance, Heart Rate Tracking for Outdoors, Black" 
+          price={40.99} 
+          image="https://images.samsung.com/is/image/samsung/uk/galaxy-note20/gallery/uk-galaxy-watch3-r840-sm-r840nzsaeua-frontmysticsilver-274582207?$PD_GALLERY_L_SHOP_JPG$" rating={5} />
+          <Product 
+          id="34324"
+          price={23.99}
+          title="Kenwood kMix stand Mixer for Baking,
+          Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Liter
+          Glass Bowl"
+          rating={4}
+          image="https://johnlewis.scene7.com/is/image/JohnLewis/238134095?$rsp-pdp-port-1080$"
+
+          />
+      
+          <Product 
+          id="34324"
+          price={23.99}
+          title="Kenwood kMix stand Mixer for Baking,
+          Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Liter
+          Glass Bowl"
+          rating={4}
+          image="https://johnlewis.scene7.com/is/image/JohnLewis/238134095?$rsp-pdp-port-1080$"
+
+          />
           </div>
 
           <div className="home__row">
-          <Product/>
+          <Product
+          id="12342142"
+           title="Pinch of Nom Everyday Light: 100 Tasty, Slimming Recipes All Under 400 Calories Hardcover" 
+          price={29.99} 
+          image="https://images-na.ssl-images-amazon.com/images/I/51hlRhTeRkL._SX369_BO1,204,203,200_.jpg" rating={5} />
+          <Product 
+          id="34324"
+          price={23.99}
+          title="Kenwood kMix stand Mixer for Baking,
+          Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Liter
+          Glass Bowl"
+          rating={4}
+          image="https://johnlewis.scene7.com/is/image/JohnLewis/238134095?$rsp-pdp-port-1080$"
+
+          />
           </div>
           </div>
       </div>
