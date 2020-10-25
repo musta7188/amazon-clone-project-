1c8558d6c22c8b517b01c863f41c297c3c03aaf6
musta7188 <musta7188@hotmail.it>
2020-10-25 22:07:23 +0000

    checkout impagination

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Checkout from './Checkout'
+import Checkout from './Checkout/Checkout'
 function App() {
   return (
     <Router>
---
+++ src/components/Checkout/Checkout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../styles/Checkout.css";
+export default function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout_left">
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/gc/2017/ASV/TopUp_1500x150.png"
+          alt="checkout banner"
+        />
+
+        <div>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <h2>The subtotal will go here</h2>
+      </div>
+    </div>
+  );
+}
