1c031225e2510de45c0db632cf4634eb71de2eaf
musta7188 <musta7188@hotmail.it>
2020-11-29 19:43:52 +0000

    dispaly time and order id in the order component

--- src/components/Orders/Order.js
+++ src/components/Orders/Order.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "../../styles/Orders.css";
+import moment from "moment";
 
-export default function Order() {
+export default function Order({ order }) {
   return (
-    <div>
-      
+    <div className="order">
+      <h2>Order</h2>
+      <p>{moment.unix(order.data.created).format("MMM Do YYYY, h:mma")}</p>
+      <p className="order__id">
+        <small>{order.id}</small>
+      </p>
     </div>
-  )
+  );
 }
