b8a6902b08a730880703bd9c118194a323f09721
musta7188 <musta7188@hotmail.it>
2020-11-29 19:38:33 +0000

    bug in props stat fixed and order component created

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,38 +1,56 @@
-import React, {useEffect, useState} from 'react'
-import {db} from '../../FireBase'
-import '../../styles/Orders.css'
+import React, { useEffect, useState } from "react";
+import { db } from "../../FireBase";
+import { connect } from "react-redux";
+import Order from './Order'
+import "../../styles/Orders.css";
 
-
-
-function Orders() {
-
+function Orders({user, basket}) {
   const [orders, setOrders] = useState([]);
 
-
-  useEffect(() =>{
-    db.collection('users')
-    .doc(user?.uid)
-    .collection('orders')
-    .orderBy('created', 'desc')
-    .onSnapshot(snapshot =>(
-      setOrders(snapshot.docs.map(doc =>({
-        id: doc.id,
-        data: doc.data()
-      })))
-    ))
-  },[])
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    } else{
+      setOrders([])
+    }
+  }, []);
   return (
     <div className="orders">
       <h1>YOu Ordards</h1>
+      <div className="orders__order">
+        {
+          orders?.map(order => (
+            <Order order={order}/>
+          ))
+        }
+      </div>
+
     </div>
-  )
+  );
 }
 
+// const mapDispatchToProps = (dispatch) => {
+//   // return {
+//   //   userStateValue: (userObject) =dispatch({ type: "USER_STATUS", payload: userObject }),
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
+const mapStateToProps = (state) => {
   return {
-    userStateValue: (userObject) => dispatch({ type: "USER_STATUS", payload: userObject }),
+    user: state.user,
+    basket: state.basket,
   };
 };
 
-export default connect(null, mapDispatchToProps)(Orders);
+export default connect(mapStateToProps)(Orders);
