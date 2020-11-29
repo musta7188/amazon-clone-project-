0f0b87424886f2708be84324ec216a201e74ad41
musta7188 <musta7188@hotmail.it>
2020-11-29 18:21:02 +0000

    stripe end point connect to the backend

--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,8 @@ const express = require('express')
 const cors = require("cors");
 const { response } = require('express');
 
-const stripe = require("stripe")
-(
-  "sk_test_BFUmB4YGEastZm4iGmlN4SHg00aFmVgsK4"
+const stripe = require("stripe")(
+  "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"
 )
 
 // API
@@ -20,8 +19,24 @@ app.use(express.json());
 // -- API routes
 app.get('/', (request, response) => response.status(200).send("works"))
 
+app.post('/payments/create', async (request, response) =>{
+    const total = request.query.total;
+    console.log('Payment Request Received ', total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total, ///subunits of the currency
+      currency: "usd",
+    })
+
+    response.status(201).send({
+      clientSecret: paymentIntent.client_secret
+    })
+})
+
+
+
 // - Listen Command
 
 exports.api = functions.https.onRequest(app)
 
-///http://localhost:5001/challenge-4b2b2/us-central1/api
+//http://localhost:5001/clone-project-54181/us-central1/api
